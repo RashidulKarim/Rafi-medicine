af0fd8507372346733a71c811959794385e73549
rashidulkarim <rashidul.karim7@gmail.com>
2021-10-18 04:06:08 +0600

    implement route and install nessary depen..

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import AboutUs from './Pages/AboutUS/AboutUs';
+import ContactUs from './Pages/ContactUs/ContactUs';
+import Home from './Pages/Home/Home/Home';
+import Login from './Pages/Login/Login';
+import NotFound from './Pages/NotFound/NotFound';
+import Ragistration from './Pages/Registration/Ragistration';
+import Footer from './Pages/Shared/Footer/Footer';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Header></Header>
+      <Switch>
+        <Route exact path='/'>
+          <Home></Home>
+        </Route>
+        <Route path='/home'>
+          <Home></Home>
+        </Route>
+        <Route path='/login'>
+          <Login></Login>
+        </Route>
+        <Route path='/ragister'>
+          <Ragistration></Ragistration>
+        </Route>
+        <Route path='/about'>
+          <AboutUs></AboutUs>
+        </Route>
+        <Route path='/contact'>
+          <ContactUs></ContactUs>
+        </Route>
+        <Route path='*'>
+          <NotFound></NotFound>
+        </Route>
+      </Switch>
+      <Footer></Footer>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Pages/Registration/Ragistration.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Ragistration = () => {
+    return (
+        <div>
+            this is ragistration
+        </div>
+    );
+};
+
+export default Ragistration;
